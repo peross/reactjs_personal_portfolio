37128050fdb826620230ee117ae56ad5d4e039c0
Pero <peross>
2021-10-26 21:22:49 +0200

    Text changed for projects section

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ export const projectData = [
     {
         id: 1,
         title: "sad",
-        about: "This is a random project I am using to illustrate in my video. This channel overreacted and I hope you are enjoying this video. Make sure you look like and comment if you have any doubt.",
+        about: "This is a random project This is a random project This is a random project This is a random project This is a random project This is a random project This is a random project ",
         tags: ["ReactJS","HTML", "CSS","Sass","JavaScript"],
         demo: "www.google.com",
         github: "www.google.com",
@@ -11,7 +11,7 @@ export const projectData = [
     {
         id: 2,
         title: "das",
-        about: "This is a random project I am using to illustrate in my video. This channel overreacted and I hope you are enjoying this video. Make sure you look like and comment if you have any doubt.",
+        about: "This is a random project This is a random project This is a random project This is a random project This is a random project This is a random project This is a random project ",
         tags: ["ReactJS","HTML", "CSS","Sass","JavaScript"],
         demo: "www.google.com",
         github: "www.google.com",
